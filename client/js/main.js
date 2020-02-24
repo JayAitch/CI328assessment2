052c7baa1d5efdb5ec7697f02f2eaab85f10e764
Jordan <jordanjharrison@hotmail.co.uk>
2020-02-24 17:02:02 +0000

    like this?

--- client/js/main.js
+++ client/js/main.js
@@ -14,9 +14,3 @@ window.addEventListener('load', (event) => {
     canvas = game.canvas;//temp
     canvasContext = canvas.getContext('2d');
 });
-
-// //temp: invert canvas to make the player being played at the bottom
-// // this will break click events as the location of clicks doesnt change!!!!
-// function rotateCanvas(playerNumber){
-//     canvas.style = "transform: rotate(" + (playerNumber - 1) * 90 + "deg)";
-// }
