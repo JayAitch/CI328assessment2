60b2f9290ae31335d38873d5e2b1867a687ce203
BradleyTesterSpratt <bradley.tester-spratt@bbc.co.uk>
2020-03-05 16:00:58 +0000

    refactor to allow different sprite to be used dynamically

--- client/js/game.js
+++ client/js/game.js
@@ -43,9 +43,9 @@ class GameScene extends Phaser.Scene {
         this.createEmitter();
 
         this.characters = {
-            "BIG": {size: 8, eyes: 4, colour: 0x00ffff},
-            "MEDIUM": {size: 3, eyes: 3, colour: 0xff0000},
-            "SMALL": {size: 0, eyes: 4, colour: 0xffff00}
+            "BIG": {size: 8, eyes: 4, colour: 0x00ffff, type: 'slime'},
+            "MEDIUM": {size: 3, eyes: 3, colour: 0xff0000, type: 'slime'},
+            "SMALL": {size: 0, eyes: 4, colour: 0xffff00, type: 'slime'}
         }
     }
 
--- client/js/player.js
+++ client/js/player.js
@@ -3,6 +3,7 @@ class Player {
     let size = character.size;
     let eyes = character.eyes;
     let colour = character.colour;
+    let type = character.type.toLowerCase();
     this.game = game;
     let bodyContainer = this.game.add.container(x, y);
     this.bodyContainer = bodyContainer;
@@ -10,7 +11,7 @@ class Player {
     this.eyeContainter = eyeContainter;
     let socketContainer = this.game.add.container(0,0);
     this.socketContainer = socketContainer;
-    this.generateBody(size, eyes, colour, this.isRotated(x, y));
+    this.generateBody(type, size, eyes, colour, this.isRotated(x, y));
 
     // this.previousX = 0;
     // this.previousY = 0
@@ -25,12 +26,12 @@ class Player {
     return 0;
   }
 
-  generateBody(size, eyes, colour, rotation) {
-    let xPos = this.placeSprites(size, 38, 0, 'slimeMiddle', this.bodyContainer, colour);
-    let leftEdge = this.game.add.sprite(xPos * -1,0,'slimeLeft').setOrigin(0.5,0.5);
+  generateBody(type, size, eyes, colour, rotation) {
+    let xPos = this.placeSprites(size, 38, 0, `${type}Middle`, this.bodyContainer, colour);
+    let leftEdge = this.game.add.sprite(xPos * -1,0,`${type}Left`).setOrigin(0.5,0.5);
     leftEdge.tint = colour;
     this.bodyContainer.add(leftEdge);
-    let rightEdge = this.game.add.sprite(xPos,0,'slimeRight').setOrigin(0.5,0.5);
+    let rightEdge = this.game.add.sprite(xPos,0,`${type}Right`).setOrigin(0.5,0.5);
     rightEdge.tint = colour;
     this.bodyContainer.add(rightEdge);
     this.placeSprites(eyes, 32, -20, 'eye', this.eyeContainter);
@@ -88,7 +89,7 @@ class Player {
       case (x < newX || y < newY):
         this.animateMovement('Right');
         break;
-      case (x == newX || y == newY):
+      case (x == newX && y == newY):
         this.animateMovement('Idle');
         break;
       case (x > newX || y > newY):
